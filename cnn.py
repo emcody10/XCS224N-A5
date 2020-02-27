85338c1c99689d338e502939932bc0414fa21571
Emily Seminerio <ecody@Emilys-MacBook-Pro.local>
2020-02-27 16:32:10 -0500

    part 1

--- cnn.py
+++ cnn.py
@@ -1,8 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
 
 ### YOUR CODE HERE for part 1e
+class CNN(nn.Module):
+    """
+    Convolutional network
+    """
+    def __init__(self, char_embedding_size, num_filters, max_word_length, kernel_size=5):
+        """
+        Initialize CNN
+        Args:
+            char_embedding_size: embedding size of characters in a word
+            num_filters: number of output channels
+            max_word_length: maximum word length
+            kernel_size: convolution window size
+        """
+        super(CNN, self).__init__()
+        self.char_embedding_size = char_embedding_size # input channels
+        self.kernel_size = kernel_size
+        self.num_filters = num_filters # output channels
+        self.max_word_length = max_word_length
+        self.conv = nn.Conv1d(char_embedding_size, num_filters, kernel_size)
+        self.maxpool = nn.MaxPool1d(max_word_length-kernel_size+1)
 
+    def forward(self, input):
+        """
+        Take batch of character embeddings, compute word embedding
+        Args:
+            input: (batch_size, char_embed_size, max_word_length)
 
+        Returns:
+            x_out: (batch_size, word_embed_size)
+
+        """
+        x_conv = self.conv(input)
+        #x_out = self.maxpool(F.relu(x_conv)).squeeze() # (batch_size, word_embed_size)
+        x_out = torch.max(F.relu(x_conv), dim=2)[0]
+        return x_out
 ### END YOUR CODE
-
--- highway.py
+++ highway.py
@@ -1,8 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
 
 ### YOUR CODE HERE for part 1d
+class Highway(nn.Module):
+    """
+    Highway network
+    Maps x_conv_out to x_highway
+    """
+    def __init__(self, word_embed_size):
+        """
+        Args:
+            word_embed_size: embedding size for input and output
+        """
+        super(Highway, self).__init__()
+        self.word_embed_size = word_embed_size
+        self.proj = nn.Linear(self.word_embed_size, self.word_embed_size)
+        self.gate = nn.Linear(self.word_embed_size, self.word_embed_size)
 
+    def forward(self, x_conv):
+        '''
+        Compute x_highway on minibatch of convolution output
+        Args:
+            x_conv: batch_size x word_embed_size
+
+        Returns:
+            x_highway: batch_size x word_embed_size
+
+        '''
+        x_proj = F.relu(self.proj(x_conv))
+        x_gate = torch.sigmoid(self.gate(x_conv))
+        x_highway = x_gate * x_proj + (1-x_gate)*x_conv
+        return x_highway
 
 ### END YOUR CODE 
 
